0e04d9c21da0967b1625922aceef4965a4536841
GOKUL P <gokulpulikkal@rediff.com>
2017-09-10 10:24:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,13 @@
 //counter code
 var button=document.getElementById('counter');
 var counter= 0;
+button.onclick =  function () {
+    //make a request to the counter end point
+    
+    //recieve the response and store it in a variable
+    
+    //render the variable in a correct span
+    counter = counter + 1;
+    var span=document.getElementById('count');
+    span.innerHTML = counter.toString();
+};
