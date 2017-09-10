10fa41e8089f34b842b650586107d145eb7904ef
GOKUL P <gokulpulikkal@rediff.com>
2017-09-10 10:42:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,22 @@
 //counter code
 var button=document.getElementById('counter');
-var counter= 0;
+
 button.onclick =  function () {
-    //make a request to the counter end point
+    //create request object
+    var request = new XMLHttpRequest();
     
     //recieve the response and store it in a variable
-    
-    //render the variable in a correct span
-    counter = counter + 1;
-    var span=document.getElementById('count');
-    span.innerHTML = counter.toString();
+    request.onreadystatechange = function (){
+      if( request.readyState === XMLHttpRequest.DONE){
+          if(request.status === 200){
+              var counter = request.responseText;
+              var span=document.getElementById('count');
+              span.innerHTML = counter.toString();
+              
+          }
+      }  
+    };
+    //make the request
+    request.open('GET','http://gokulpulikkal.imad.hasura-app.io',true);
+    request.send(null);
 };
